3bd5c0dde2e4a29259104264c706b823877989bf
imrojhimal <e193021@ugrad.iiuc.ac.bd>
2024-02-23 01:55:17 +0600

    changing color in home

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+       backgroundColor: Color.fromARGB(255, 170, 38, 175), 
         body: Center(
           child: Text("hello world"),
         ),
