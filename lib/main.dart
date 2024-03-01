49445aad99d6bb4ea2162a27956c3cc35d4e6c91
imrojhimal <e193021@ugrad.iiuc.ac.bd>
2024-03-02 02:04:52 +0600

    changing the allignment of the gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
@@ -8,8 +10,10 @@ void main() {
         body: Container(
           decoration:const BoxDecoration(
             gradient: LinearGradient(
-             colors:[Color.fromARGB(255, 193, 186, 194),
-             Color.fromARGB(255, 220, 211, 226)]
+             colors:[Color.fromARGB(255, 63, 28, 152),
+             Color.fromARGB(255, 32, 8, 75)],
+             begin:Alignment.topLeft,
+             end:Alignment.bottomLeft,
             )
           ),
           child: const Center(
