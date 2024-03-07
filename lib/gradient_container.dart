b6df014536cd83abf2ad876d5f66a223381ef830
imrojhimal <e193021@ugrad.iiuc.ac.bd>
2024-03-07 20:07:15 +0600

    adding const at variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/styletext.dart';
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomLeft;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomLeft;
 class GradientContainer extends StatelessWidget{
  const  GradientContainer({super.key});
   @override
   Widget build(context)
   {
     return Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-             colors:const 
+             colors: 
              [Color.fromARGB(255, 50, 19, 163),
-             Color.fromARGB(255, 135, 130, 145)],
+             Color.fromARGB(255, 35, 17, 69)],
              begin:startAlignment,
              end:endAlignment,
             )
