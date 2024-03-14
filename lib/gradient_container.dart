c9f5032bb4f8d1f21266a7dfcc6cff990766ca2a
imrojhimal <e193021@ugrad.iiuc.ac.bd>
2024-03-14 12:54:41 +0600

    changing the text

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -19,7 +19,7 @@ class GradientContainer extends StatelessWidget{
             )
           ),
           child: const Center(
-            child:StyleText("Sakib the goyal") ,
+            child:StyleText("cce female batch ") ,
         ),
       );
   }
